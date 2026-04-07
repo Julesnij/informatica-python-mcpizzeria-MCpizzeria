2f8d5f58004f9b7edb38283b0c0f159d4bab37ff
Julesnij <julesnijkamp08@gmail.com>
2026-04-07 08:58:53 +0200

    Tabellen toegevoegd

--- MCPizzeriaSQL.py
+++ MCPizzeriaSQL.py
@@ -16,6 +16,16 @@ with sqlite3.connect("MCPizzeria.db") as db:
 
 ### ---------  Functie definities  -----------------
 
+def maakTabellenAan(): 
+# Maak een nieuwe tabel met 3 kolommen: id, naam, prijs 
+cursor.execute(""" 
+    CREATE TABLE IF NOT EXISTS tbl_pizzas( 
+        gerechtID INTEGER PRIMARY KEY AUTOINCREMENT, 
+        gerechtNaam TEXT NOT NULL, 
+        gerechtPrijs REAL NOT NULL);""") 
+
+print("Tabel 'tbl_pizzas' aangemaakt.") 
+
 
 ### --------- Hoofdprogramma  ---------------
 
